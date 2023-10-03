ab612b7b1885f42e40a8ba54785f5ee8406469c8
FelixSvenHarry <>
2023-10-03 14:52:14 +0000

    add logic to print game board as well as coordinates, 1 to 8 and A to H.

--- run.py
+++ run.py
@@ -9,3 +9,18 @@ class Board:
         self.size = size
         self.hidden_pattern = [[' ']*size for _ in range(size)]
         self.guess_pattern = [[' ']*size for _ in range(size)]
+
+    def print_board(self):
+        """
+        Prints the game board, with rows labeled
+        1 to 8 and columns labeled "A" to "H".
+        """
+        print(' A B C D E F G H')
+        print(' ***************')
+        row_num = 1
+        for guess_row in self.guess_pattern:
+            print("%d|%s|" % (row_num, "|".join(guess_row)))
+            row_num += 1
+    
+    
+
